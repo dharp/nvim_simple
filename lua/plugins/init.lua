f60395cd26934d61abd6ba0319e5104b070ff814
Dylan Robert Harp <dharp73@gmail.com>
2026-04-27 10:04:04 -0600

    Trying out claudecode plugin instead of avante

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,24 +39,49 @@ return {
     "preservim/tagbar",
     lazy = false,
   },
+  -- {
+  --   "yetone/avante.nvim",
+  --   lazy = false,
+  --   --event = "VeryLazy",
+  --   build = "make",
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --   },
+  --   opts = {
+  --     provider = "claude",
+  --     providers = {
+  --       claude = {
+  --         model = "claude-opus-4-6",
+  --       },
+  --     },
+  --   },
+  -- },
   {
-    "yetone/avante.nvim",
+    "coder/claudecode.nvim",
+    dependencies = { "folke/snacks.nvim" },
     lazy = false,
-    --event = "VeryLazy",
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      provider = "claude",
-      providers = {
-        claude = {
-          model = "claude-opus-4-6",
-        },
+    config = true,
+    keys = {
+      { "<leader>a", nil, desc = "AI/Claude Code" },
+      { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+      { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+      { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+      { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+      { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+      { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+      {
+        "<leader>as",
+        "<cmd>ClaudeCodeTreeAdd<cr>",
+        desc = "Add file",
+        ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },
       },
+      -- Diff management
+      { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+      { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
     },
   },
 }
